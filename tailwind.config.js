83085bfd3cdf8adbb00e52d7f53237eb2ded7292
Melina Gonzalez <melina@gomelina.me>
2021-07-06 22:14:55 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+function withOpacity(variableName) {
+  // const hsler = /hsl\((.+)\)/i
+
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `hsla(var(${variableName}), ${opacityValue})`
+    }
+
+    return `hsl(var(${variableName}))`
+  }
+}
+
+module.exports = {
+  purge: {
+    content: [
+      "./layouts/**/*.html",
+      "./content/**/*.md",
+      "./content/**/*.html"
+    ],
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Inter']
+      },
+      colors: {
+        'th-primary': withOpacity("--primary"),
+        'th-secondary': withOpacity("--secondary"),
+        'th-input': withOpacity("--input"),
+        'th-muted': withOpacity("--muted"),
+        'th-highlight': withOpacity("--highlight"),
+        'th-background': withOpacity("--background")
+      },
+    },
+  },
+  variants: {
+    extend: {
+	  scale: ['hover'],
+	  hueRotate: ['hover'],
+	  gap: ['hover'],
+	  space: ['hover']
+	},
+  },
+  plugins: [],
+};
